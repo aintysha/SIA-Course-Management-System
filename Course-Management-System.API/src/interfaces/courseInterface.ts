4e7036835a70e3f6ff884946fe9e336636b3ea3d
Trisha Marie <abelgasmarie337@gmail.com>
2024-12-10 04:38:03 +0800

    Commit Changes

--- Course-Management-System.API/src/interfaces/courseInterface.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from "mongoose";
-
-export interface ICourse extends Document {
-  Course_ID : Number;
-  Course_name : String;
-  Credits : Number;
-  Catalog_no : String;
-  Academic_yr : Number;
-} 
